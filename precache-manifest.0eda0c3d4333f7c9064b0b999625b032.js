6f9ca235266fb78a6080793b24ed7fed069471a8
Ashley Benntt <ashleyben21@gmail.com>
2019-06-24 17:01:00 +0100

    Updates

---
+++ precache-manifest.0eda0c3d4333f7c9064b0b999625b032.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4574023aac00da22868dd28276b4f62b",
+    "url": "/star-wars-characters-database/index.html"
+  },
+  {
+    "revision": "f6d03e208920bda876ef",
+    "url": "/star-wars-characters-database/static/css/main.09407893.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/star-wars-characters-database/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "f6d03e208920bda876ef",
+    "url": "/star-wars-characters-database/static/js/main.46242584.chunk.js"
+  },
+  {
+    "revision": "f3046ce506611c40baee",
+    "url": "/star-wars-characters-database/static/js/runtime~main.f86f3579.js"
+  }
+]);
